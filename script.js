d83508ed8dd82f69f2123e1e19f56562f068dfb1
Jair Barbosa Santana <jairbse7@gmail.com>
2026-06-06 02:48:30 -0300

    Gráfico de Pizza 100%

--- script.js
+++ script.js
@@ -24,15 +24,35 @@ let item = document.createElement("li")
 item.textContent = `${descricao} - R$ ${valor} (${categoria})`
 document.getElementById("lista").appendChild(item)
 
+//Variáveis das Categorias
+let totalAlimentacao = 0
+let totalTransporte = 0
+let totalLazer = 0
+
 //Soma todos os valores
 let total = 0
 
+//Percorre o Array
 for (let i = 0; i < gastos.length; i++){
     total = total + gastos[i].val
+    //Se a categoria for Alimentação soma a sua variável
+    if(gastos[i].cat === "Alimentação"){
+    totalAlimentacao = totalAlimentacao + gastos[i].val
+    //Se a categoria for Transporte soma a sua variável
+    } else if (gastos[i].cat === "Transporte"){
+    totalTransporte = totalTransporte + gastos[i].val
+    //Se a categoria for Lazer soma a sua variável
+    } else if (gastos[i].cat === "Lazer"){
+    totalLazer = totalLazer + gastos[i].val
+    } 
 }
 
 //Atualiza total no HTML
 document.getElementById("total").textContent = total
+
+//Atualiza o Gráfico de Pizza
+grafico.data.datasets[0].data = [totalAlimentacao, totalTransporte, totalLazer]
+grafico.update()
 })
 
 // Gráfico de Pizza
